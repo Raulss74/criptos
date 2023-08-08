568574370c4987c7d55c525fce598d8802adaa46
Raulss74 <68406050+Raulss74@users.noreply.github.com>
2023-08-08 11:59:34 -0600

    actualizacion

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
 import { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import Error from './error'
